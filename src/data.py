9c2af946599fec116162fee7edea121f0c09f3e5
patrick <visioninghail32@gmail.com>
2025-01-21 21:54:26 +0000

    work in progress

--- src/data.py
+++ src/data.py
@@ -1,11 +1,20 @@
+#%%
+
+import matplotlib.pyplot as plt
+
+# neural net imports
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
 from torch import float32
-from torch.utils.data import random_split, Dataset
+from torch.utils.data import random_split, Dataset, DataLoader
 
 from torchvision.transforms import v2 as transforms
 from torchvision import datasets
 
 # TODO add environment variables for these
-DATA_ROOT = "data/"
+DATA_ROOT = "../data/"
 TRAINING_PERCENT = 0.7
 
 # TODO finalise the transformations we'll be using
@@ -30,9 +39,59 @@ TRANSFORM = transforms.Compose((
     # transforms.Normalize((0.5,), (0.5,)),
 ))
 
-def load_dataset(path = DATA_ROOT):
+# changed to init_dataset as we will load the data using LoadDataset
+# this is for batch loading
+
+def init_dataset(path = DATA_ROOT): 
     return datasets.ImageFolder(path, transform=TRANSFORM)
 
-def split_dataset(dataset: Dataset = load_dataset()):
+def split_dataset(dataset: Dataset = init_dataset()):
     train_data, test_data = random_split(dataset, [TRAINING_PERCENT, 1 - TRAINING_PERCENT])
     return train_data, test_data
+
+def load_data(train_data : Dataset, test_data : Dataset):
+    trainloader = DataLoader(train_data, batch_size=4,
+                            shuffle=True, num_workers=2)
+    testloader = DataLoader(test_data, batch_size=4,
+                            shuffle=True, num_workers=2)
+    return trainloader, testloader
+
+train_data, test_data = split_dataset()
+trainloader, testloader = load_data(train_data, test_data)
+
+train_features, train_labels = next(iter(trainloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0] 
+label = train_labels[0] 
+
+img = img.expand(3, -1, -1)
+plt.imshow(img.permute(1, -1, 0)) # this is necessary to transpose the size shape from (1, 28, 28) to (28, 28, 1)
+plt.show()
+# %%
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
