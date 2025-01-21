4e1a7369053672442743533a833d3eefced784bd
blue <midnight@nightmarish.blue>
2025-01-21 16:39:15 +0000

    Create functions to load the dataset and split it

--- src/data.py
+++ src/data.py
@@ -1,25 +1,33 @@
 from torch import float32
-from torch.utils.data import random_split
+from torch.utils.data import random_split, Dataset
 
 from torchvision.transforms import v2 as transforms
 from torchvision import datasets
 
-# TODO turn these into environment variables, or better yet parameters
+# TODO add environment variables for these
 DATA_ROOT = "data/"
 TRAINING_PERCENT = 0.7
 
 # TODO finalise the transformations we'll be using
-transform = transforms.Compose((
+TRANSFORM = transforms.Compose((
     # convert the PIL image into a tensor
     transforms.ToImage(),
     transforms.ToDtype(float32, scale=True),
 
-    transforms.Grayscale(), # drop the unneeded dimensions
+    # images do not need colour
+    transforms.Grayscale(), # converts from 3ch RGB to 1ch grayscale
+    # lambda i: squeeze(i, dim=0) # removes the 'vestigial' dimension, leaving behind a 2D matrix
+
+    # resize to the necessary size?
+
     # transforms.RandomResizedCrop(), # find out how to use this
     # normalising images is done on photos of the real world; on handwriting this is not as necessary
     # transforms.Normalize((0.5,), (0.5,)),
 ))
 
-dataset = datasets.ImageFolder(DATA_ROOT, transform=transform) # the entire dataset, preprocessed
+def load_dataset(path = DATA_ROOT):
+    return datasets.ImageFolder(path, transform=TRANSFORM)
 
-train_data, test_data = random_split(dataset, [TRAINING_PERCENT, 1 - TRAINING_PERCENT])
+def split_dataset(dataset: Dataset = load_dataset()):
+    train_data, test_data = random_split(dataset, [TRAINING_PERCENT, 1 - TRAINING_PERCENT])
+    return train_data, test_data
