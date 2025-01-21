e70c9aecd35e268cf6e5dd071c77cc1f801d3d3c
patrick <visioninghail32@gmail.com>
2025-01-21 01:24:20 +0000

    loaded files into pytorch

--- pytorch-start.py
+++ pytorch-start.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from PIL import Image
+from torch.utils.data import DataLoader
 
 import torchvision
 import torchvision.transforms as transforms
@@ -17,10 +18,20 @@ transform = transforms.Compose(
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 trainset = torchvision.datasets.ImageFolder(root="./data", transform=transform)
+datatest = DataLoader(trainset, batch_size=64, shuffle=True)
+
+# code taken from https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+train_features, train_labels = next(iter(datatest))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0]
+img = img.permute(*torch.arange(img.ndim - 1, -1, -1)) # this is necessary to transpose the size shape from (3, 28, 28) to (28, 28, 3)
+label = train_labels[0]
+plt.imshow(img)
+plt.show()
+print(f"Label: {label}")
 
 # testing code
 item = trainset[26900]
 print(item)
-
-
 
