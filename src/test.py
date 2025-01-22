d9cf3eae998f897b197a5c6e2eac383577568e9b
blue <midnight@nightmarish.blue>
2025-01-22 14:45:38 +0000

    Put test in its own file

---
+++ src/test.py
@@ -0,0 +1,18 @@
+from torch import nn, inference_mode
+from torch import max as torch_max
+from torch.utils.data import DataLoader
+
+def test(net: nn.Module, data_loader: DataLoader):
+    correct = 0
+    total = 0
+    with inference_mode():
+        for data in data_loader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch_max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on test images: %d %%' % (
+        100 * correct / total))
+    
