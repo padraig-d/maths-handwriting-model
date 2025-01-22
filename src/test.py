70fdd3f7f9133fed08c4d2e06a6334657b1a1277
blue <midnight@nightmarish.blue>
2025-01-22 20:31:04 +0000

    test() returns the results, and runner uses that

--- src/test.py
+++ src/test.py
@@ -1,18 +1,31 @@
-from torch import nn, inference_mode
+from torch import nn, inference_mode, Tensor
 from torch import max as torch_max
 from torch.utils.data import DataLoader
 
+import numpy as np
+
 def test(net: nn.Module, data_loader: DataLoader):
-    correct = 0
-    total = 0
+    max_size = len(data_loader) * (data_loader.batch_size) # the max number of entries we'll need to store
+    truth = np.empty(dtype=np.int64, shape=max_size) # this is the datatype of labels
+    predictions = np.empty(dtype=np.int64, shape=max_size)
+
+    size = 0
     with inference_mode():
         for data in data_loader:
+            images: Tensor
+            labels: Tensor
             images, labels = data
+            length = len(labels) # should be the same every time, except the last
+
+            truth[size:size + length] = labels.cpu()
+            # truth.put(size, labels.cpu())
+
             outputs = net(images)
             _, predicted = torch_max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            predictions[size:size + length] = predicted.cpu()
+            # predictions.put(size, predicted.cpu())
 
-    print('Accuracy of the network on test images: %d %%' % (
-        100 * correct / total))
-    
+            size += length
+    truth.resize(size, refcheck=False) # disable ref checking because the debugger triggers the error
+    predictions.resize(size, refcheck=False)
+    return truth, predictions
